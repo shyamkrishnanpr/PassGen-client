f9f45a20f7166aed04f05cdeedc85ab6c3d0bbd1
shyam krishnan <shyamkrishnasby@gmail.com>
2023-10-17 20:52:38 +0530

    url updated

--- src/Axios/axios.ts
+++ src/Axios/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://127.0.0.1:3010/api",
+  // baseURL: "http://127.0.0.1:3010/api",
+  baseURL:"https://safekey.onrender.com/api"
 });
 
 instance.interceptors.request.use(
